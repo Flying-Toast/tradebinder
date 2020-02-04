c4f138ea036f567c11ba9aae5e0e57d1872d8a18
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-02-04 18:58:44 -0500

    set_symbol template tag

--- cards/templatetags/mtg.py
+++ cards/templatetags/mtg.py
@@ -17,7 +17,7 @@ def render_code(code):
     if "/" in code:
         code = code.replace("/", "")
     url = static(escape_uri_path(f"cards/symbols/{code}.svg"))
-    return f'<img class="mana-symbol" src="{url}" alt="{escape(f"{{{alt_code}}}")}">'
+    return f'<img class="card-symbol mana-symbol" src="{url}" alt="{escape(f"{{{alt_code}}}")}">'
 
 
 @register.filter
@@ -54,3 +54,13 @@ def card_img(card):
     if not isinstance(card, models.Card):
         raise TypeError("The card_img tag requires a 'cards.models.Card' as the argument")
     return static(escape_uri_path(f"cards/card_imgs/{card.id}.jpg"))
+
+
+@register.simple_tag
+def set_symbol(abbrev, rarity=None):
+    if rarity is not None and rarity not in ['c', 'u', 'r', 'm']:
+        raise RuntimeError("set_symbol tag requires 'rarity' (optional) be one of: 'c', 'u', 'r', 'm'")
+    url = static(escape_uri_path(f"cards/set_imgs/{abbrev.lower()}.svg"))
+    return mark_safe(
+        f'<img class="card-symbol set-symbol{" rarity-"+rarity if rarity else ""}" src="{url}" alt="{escape(abbrev)}">'
+    )
