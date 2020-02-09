1d517a48dfbc4126b29002483f21db3ed825cfd6
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-02-09 18:37:44 -0500

    Trade binder model

--- trading/models.py
+++ trading/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class User(AbstractUser):
     pass
+
+
+class Binder(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
